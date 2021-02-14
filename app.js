06339ec0ee0113bc8bc71e5ad3cb9ca67865a472
facuNorie <noriegafacundo08@gmail.com>
2021-02-13 22:20:27 -0300

    agregando funcionalidad a la calculadora (solo con 2 valores)

--- app.js
+++ app.js
@@ -8,19 +8,53 @@ for (let i = 0; i < contentButton.length; i++) {
     if(contentButton[i] == "+" ||contentButton[i] == "-"||contentButton[i] == "*"||contentButton[i] == "/"){
         btn.classList.add("calculadora__signal");
     }
+    if(contentButton[i] == "=") btn.classList.add("calculadora__igual");
     document.querySelector(".calculadora__div-buttons").appendChild(btn);
 }
 
 
-
 // Dark Mode
-
 let darkMode = document.querySelector(".checkbox");
 
 darkMode.addEventListener("click",()=>{
     document.querySelectorAll(".calculadora__btn").forEach(element =>{
         element.classList.toggle("darkMode");
     });
+    document.querySelectorAll(".calculadora__signal").forEach(element =>{
+        element.classList.toggle("darkSignal");
+    });
     document.querySelector(".calculadora__input").classList.toggle("darkMode");
     document.querySelector(".calculadora").classList.toggle("darkCalc");
+    document.querySelector(".calculadora__igual").classList.toggle("darkIgual");
+
 });
+
+//Imprimiendo en pantalla los numeros
+
+document.querySelectorAll(".calculadora__btn").forEach(element =>{
+        element.addEventListener("click",()=>{
+            let input = document.querySelector(".calculadora__input");
+            if(element.textContent == "="){
+                input.value = mostrarResultado(input.value);
+                return;
+                
+            }
+            input.value += element.textContent;
+        });
+});
+
+const mostrarResultado = valor =>{
+    if(valor.includes("+")){
+        let newValor = valor.split("+");
+        return parseInt(newValor[0]) + parseInt(newValor[1]);
+    }else if(valor.includes("-")){
+        let newValor = valor.split("-");
+        return parseInt(newValor[0]) - parseInt(newValor[1]);
+    }else if(valor.includes("*")){
+        let newValor = valor.split("*");
+        return parseInt(newValor[0]) * parseInt(newValor[1]);
+    }else if(valor.includes("/")){
+        let newValor = valor.split("/");
+        return parseInt(newValor[0]) / parseInt(newValor[1]);
+    }
+} 
